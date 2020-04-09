3e5b9f48c82269591f540509f2b0e298111dfe7e
bteixeira <bteixeira@DCCNOTE135>
2020-04-09 15:29:30 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './css/style.css'
 
 function App() {
   const [repositories, setRepositories] = useState({
-    aeroportos: 5,
-    nuvens: 8,
-    area: '10x10',
   });
 
   async function loadRepositories() {
-    const response = await fetch(`http://localhost:3030/?a=${repositories.aeroportos}&n=${repositories.nuvens}&t=${repositories.area}`);
-    const data = await response.json();
+    const response = await axios.post(`http://localhost:3030/?`,
+      {
+        aeroporto: repositories.aeroportos,
+        nuvens: repositories.nuvens,
+        terreno: repositories.area
+      });
+
+    const data = await response.data;
 
     setRepositories({ ...repositories, ret: data });
   }
 
-  // useEffect(() => {
-  //   loadRepositories();
-  // }, [repositories.nuvens, repositories.area, repositories.aeroportos, loadRepositories]);
+  useEffect(() => {
+    if (repositories.nuvens || repositories.area || repositories.aeroportos) {
+      loadRepositories();
+    }
+  }, [repositories.nuvens, repositories.area, repositories.aeroportos]);
 
-  // function enviar() {
+  function enviar(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
 
-  // }
+    setRepositories({
+      ...repositories,
+      aeroportos: data.get('aeroportos'),
+      nuvens: data.get('nuvens'),
+      area: data.get('area')
+    })
+  }
 
   return (
     <>
@@ -38,19 +52,22 @@ function App() {
               </div>
             )}
           </div>
-          <p>{repositories.ret.diasParaPrimeiroAeroporto} para o primeiro aeroporto ser coberto pelas nuvens</p>
-          <p>{repositories.ret.dias} para todos aeroportos serem cobertos pelas nuvens</p>
+          <p>Levará {repositories.ret.diasAeroportoCoberto} dias para o primeiro aeroporto ser coberto pelas nuvens.</p>
+          <p>Levará {repositories.ret.dias} dias para que todos os aeroportos sejam cobertos pelas nuvens.</p>
 
         </div>}
 
       </div>
 
-      {/* <div className="form">
-        <p>Aeroportos: <input id="aeroportos" type="text" /></p>
-        <p>Nuvens: <input id="nuvens" type="text" /></p>
-        <p>Área: <input id="area" type="text" /></p>
-        <p><input type="button" onClick={() => enviar()} /></p>
-      </div> */}
+      <div className="form">
+        <form onSubmit={enviar} >
+          <p>Aeroportos: <input name="aeroportos" id="aeroportos" type="text" /></p>
+          <p>Nuvens: <input name="nuvens" id="nuvens" type="text" /></p>
+          <p>Área: <input name="area" id="area" type="text" /></p>
+          <button>Enviar</button>
+
+        </form>
+      </div>
     </>
 
   );
