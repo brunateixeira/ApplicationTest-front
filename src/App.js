3833acd682c047df01dc5d0e8312772b16a87924
bteixeira <bteixeira@DCCNOTE135>
2020-04-09 13:47:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './css/style.css'
 
 function App() {
+  const [repositories, setRepositories] = useState({
+    aeroportos: 5,
+    nuvens: 8,
+    area: '10x10',
+  });
+
+  async function loadRepositories() {
+    const response = await fetch(`http://localhost:3030/?a=${repositories.aeroportos}&n=${repositories.nuvens}&t=${repositories.area}`);
+    const data = await response.json();
+
+    setRepositories({ ...repositories, ret: data });
+  }
+
+  // useEffect(() => {
+  //   loadRepositories();
+  // }, [repositories.nuvens, repositories.area, repositories.aeroportos, loadRepositories]);
+
+  // function enviar() {
+
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="monitor">
+        {repositories.ret && <div>
+          <div className="mapas">
+            {repositories.ret.mapas.map(mapa =>
+              <div className="mapa">
+                {mapa.map(linha =>
+                  <div className="linha">{linha.map(celula =>
+                    <span className="celula">{celula}</span>
+                  )}</div>
+                )}
+              </div>
+            )}
+          </div>
+          <p>{repositories.ret.diasParaPrimeiroAeroporto} para o primeiro aeroporto ser coberto pelas nuvens</p>
+          <p>{repositories.ret.dias} para todos aeroportos serem cobertos pelas nuvens</p>
+
+        </div>}
+
+      </div>
+
+      {/* <div className="form">
+        <p>Aeroportos: <input id="aeroportos" type="text" /></p>
+        <p>Nuvens: <input id="nuvens" type="text" /></p>
+        <p>Área: <input id="area" type="text" /></p>
+        <p><input type="button" onClick={() => enviar()} /></p>
+      </div> */}
+    </>
+
   );
 }
 
